775632769bdfd016279709e4b834e58a9ff1ee53
Im-AkashKhan <kashiwtht@gmail.com>
2025-05-12 17:33:09 +0500

    Classs 01

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+// VARIABLES [ VAR LET CONST ]
+
+// let varibalName = "dsfadsf";
+//     varibalName = "66666";
+
+// variable [VARIABLE small , skdfj33]
+
+// DONT USE [const , 33 ]
+
+
+// const ID = 4;
+//  ID = 6
+// document.write(ID);
+
+// DATA TYPES [string ,  number , bolean]
+
+let firstName = "this is 234"; //String
+
+let numb1 = "35"; //NUMBER
+let num2 = 5;
+
+let Login = true / false; //Bolean
+
+
+
+
+
+document.write(numb1 + num2);
+
