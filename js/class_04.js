9ea9be8e9746f3363c8543e799ebc31552eca3da
Im-AkashKhan <kashiwtht@gmail.com>
2025-05-14 16:38:42 +0500

    Class 04 // dropdown menu

---
+++ js/class_04.js
@@ -0,0 +1,37 @@
+
+
+let a = 150;
+
+let b;
+
+// (a == 50) ? b = "true" : b = "false";
+
+// console.log(b);
+
+
+// function myFunction(){
+//     alert("Are you sure");
+// }
+
+// ARROW FUNCTION
+// myFunciton = (a , b) => {
+//     document.write( a + b );
+// }
+
+// myFunciton("66" , 10);
+// myFunciton(30 , 20);
+
+// myFunciton();
+// myFunciton();
+// myFunciton();
+// myFunciton();
+// myFunciton();
+// myFunciton();
+
+
+
+
+// javascript events
+function myAlert(){
+    alert("HHHHHHHHH");
+}
